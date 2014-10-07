807f379d2e294d92cd18784d9049aa3d41d5732a
Dan Leehr <dan.leehr@nescent.org>
2014-10-07 16:39:13 -0400

    Refactor calibration.js

--- app/models/calibration.js
+++ app/models/calibration.js
@@ -5,35 +5,44 @@ function getConnection() {
   return connection;
 }
 
-function Calibration() {
+function Calibration(databaseRow) {
+    for(var propertyName in databaseRow) {
+        this[propertyName] = databaseRow[propertyName];
+    }
+}
+
+function Calibrations() {
+  function query(queryString, queryParams, callback) {
+    var connection = getConnection();
+    connection.connect();
+    connection.query(queryString, queryParams, callback);
+    connection.end()
+  }
+
   this.findById = function(calibration_id, callback) {
-      // callback is (calibration, err)
-      // query the calibrations by id
-      var query = 'SELECT * FROM calibrations WHERE CalibrationID = ?';
-      var connection = getConnection();
-      connection.connect();
-      connection.query(query, [calibration_id], function(err, results) {
+    // callback is (calibration, err)
+    // query the calibrations by id
+    var queryString = 'SELECT * FROM calibrations WHERE CalibrationID = ?';
+    query(queryString, [calibration_id], function(err, results) {
         if(err) {
           callback(null,err);
         } else {
-          callback(results[0]);
+          var calibrationResult = new Calibration(results[0]);
+          callback(calibrationResult);
         }
       });
-      connection.end()
     };
   this.findByFilter = function(params, callback) {
-    var query = 'SELECT * FROM calibrations WHERE minAge > ? AND maxAge < ?';
-    var connection = getConnection();
-    connection.connect();
-    connection.query(query, [params.min, params.max], function(err, results) {
+    var queryString = 'SELECT * FROM calibrations WHERE minAge > ? AND maxAge < ?';
+    query(queryString, [params.min, params.max], function(err, results) {
       if(err) {
         callback(null,err);
       } else {
-        callback(results);
+        var calibrationResults = results.map(function(result) { return new Calibration(result)});
+        callback(calibrationResults);
       }
     });
-    connection.end();
   };
 }
 
-module.exports = new Calibration();
+module.exports = new Calibrations();
